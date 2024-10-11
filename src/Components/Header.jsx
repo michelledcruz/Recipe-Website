2e700ca9ca0e860580eab5275895b875006a1b7a
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2024-10-11 11:29:47 +1100

    added auth0 logout and user profile

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"; // Import Link from react-router-dom
 import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
 
   return (
     <header>
@@ -26,7 +26,28 @@ const Header = () => {
             <Link to="#contact">Contact</Link>
           </li>
           <li>
-            <button onClick={() => loginWithRedirect()}>Log In</button>
+            {isAuthenticated ? (
+              <>
+                <button
+                  onClick={() =>
+                    logout({
+                      logoutParams: { returnTo: window.location.origin },
+                    })
+                  }
+                >
+                  Log Out
+                </button>
+              </>
+            ) : (
+              <button onClick={() => loginWithRedirect()}>Log In</button>
+            )}
+          </li>
+          <li>
+            {isAuthenticated && (
+              <>
+                <p>Welcome {user.name}</p>
+              </>
+            )}
           </li>
         </ul>
       </nav>
