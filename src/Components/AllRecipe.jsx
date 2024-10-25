d91aa38df0f25d63ccbbf823d13877310788a650
Michelle Dcruz <52381792+michelledcruz@users.noreply.github.com>
2024-10-25 16:10:54 +1100

    added favourites as heart svg

--- src/Components/AllRecipe.jsx
+++ src/Components/AllRecipe.jsx
@@ -81,10 +81,23 @@ const AllRecipe = () => {
                     <p>{recipe.strMeal}</p>
                   </div>
                 </div>
-                <button onClick={() => handleFavoriteClick(recipe)}>
-                  {isFavourite(recipe.idMeal)
-                    ? "Remove from Favorites"
-                    : "Add to Favorites"}
+                <button
+                  className="favorite-btn"
+                  onClick={() => handleFavoriteClick(recipe)}
+                >
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill={isFavourite(recipe.idMeal) ? "red" : "none"}
+                    stroke="red"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="favorite-icon"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
+                  </svg>
                 </button>
               </div>
             ))}
